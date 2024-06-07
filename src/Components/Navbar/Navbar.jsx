f9c3f4b2b45c4be1fc0659b288ee0616d07ac231
xClaudiaArias <xClaudiaArias@gmail.com>
2024-06-07 14:01:36 -0400

    cast style complete.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import "./Navbar.css"
 import cloud_icon from '../../Assets/cloud-icon.png'
 import SearchBar from '../SearchBar/SearchBar'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+    let location = useLocation();
+
+    console.log(location.pathname, " location")
+
     return (
         <div className="navbar">
             <div className="logo">
@@ -13,7 +18,9 @@ const Navbar = () => {
 
 
             <div className='search'>
-                <SearchBar />
+                {
+                    (location.pathname === "/about" || location.pathname === "/contact") ? <></> : <SearchBar />
+                }
             </div>
         </div>
     )
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,33 +1,37 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <div className="left-contact">
         <h1>CONTACT</h1>
         <h2>Send me a message</h2>
 
         <form action="">
-          <label htmlFor="firstname">First name</label>
-          <input type="text" name="firstname" id="firstname" />
+          <div className="contact-inputs">
+            <label htmlFor="firstname">First name</label>
+            <input type="text" name="firstname" id="firstname" />
 
-          <label htmlFor="lastname">Last name</label>
-          <input type="text" name="lastname" id="lastname" />
-
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message"></textarea>
-
+            <label htmlFor="lastname">Last name</label>
+            <input type="text" name="lastname" id="lastname" />
+          </div>
+          <br/>
+          <div className="text-area-container">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message"></textarea>
+          </div>
+          <br/>
           <button>Submit</button>
         </form>
       </div>
 
       <div className="right-contact">
-        <div className="socials">
-          socials here
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="contact-socials">
+          <div><Facebook style={{fontSize: 38, color: '#1e1e1e', cursor: 'pointer'}} /></div>
+          <div><LinkedIn style={{fontSize: 38, color: '#1e1e1e', cursor: 'pointer'}} /></div>
+          <div><Twitter style={{fontSize: 38, color: '#1e1e1e', cursor: 'pointer'}} /></div>
+          <div><Instagram style={{fontSize: 38, color: '#1e1e1e', cursor: 'pointer'}} /></div>
         </div>
       </div>
     </div>
